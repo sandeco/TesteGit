a01bd2d402a69133e2e997cb61f801b63c01f29e
Sanderson Macedo <sandecom@gmail.com>
2016-04-18 11:04:52 -0300

    alteração do arquivo principal

--- app/src/main/java/ifg/edu/br/testegit/MainActivity.java
+++ app/src/main/java/ifg/edu/br/testegit/MainActivity.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int x=0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
